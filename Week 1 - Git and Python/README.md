db2a96474a7ce17a6ccd1a3ff8772e650f599ea7
DomenickD <deothe1@gmail.com>
2024-02-09 12:07:15 -0500

    Created a python file and a subfolder README

---
+++ Week 1 - Git and Python/README.md
@@ -0,0 +1,22 @@
+#Week 1 Activities Summary
+
+This week, we focused on using STormwind to complete a python basics/fundamentals course. 
+We also focused on learning git, initializing our repositories on github and getting into a habit of using git to interact with github. 
+
+Some key notes: 
+
+- Creating a github repository
+- Pushing to our own repository
+- Pulling from a public repository
+- Branching and Merging
+- Collaboration and Version Control
+- Markdown basics
+
+
+Things we will probably get to:
+
+- `git clone`
+- `git push`
+- `git pull`
+- Managing large datasets in git
+
---
+++ Week 1 - Git and Python/helloWorld.py
@@ -0,0 +1,20 @@
+#This will be the first python program for the IEX-USF Course
+print("Hello World")
+
+#Now let's have some fun. numbers are great
+def reverse_list(list_of_numbers):
+    return list_of_numbers[::-1]
+
+numbered_list = [10,9,8,7,6,5,4,3,2,1]
+print(f"The variable numbered_list is currently: {numbered_list}")
+
+#this is the function to reverse the string with indices
+print(f"The variable reversed is: {reverse_list(numbered_list)}")
+
+#Here I used the .reverse() function to reverse the original list
+numbered_list.reverse()
+print(f"The variable reversed is: {numbered_list}")
+
+#The downside to the .reverse() function is I can't get the original list back with using this function again
+#using the reverse_list(list_of_numbers) function is more practical.
+
