a3866bd3269e1110f3733ee0380d0f4f93f6f4d5
DomenickD <deothe1@gmail.com>
2024-08-27 11:04:58 -0400

    pylint will fail

--- Docker_Compose/flask_app/app.py
+++ Docker_Compose/flask_app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
 import sqlite3
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 def query_database(query):
-    conn = sqlite3.connect('/app/titanic_data.db')  # Adjust path if needed
+    conn = sqlite3.connect("/app/titanic_data.db")  # Adjust path if needed
     cursor = conn.cursor()
     cursor.execute(query)
     columns = [description[0] for description in cursor.description]  # Get column names
@@ -12,10 +13,11 @@ def query_database(query):
     conn.close()
     return {"columns": columns, "data": data}
 
-@app.route('/query', methods=['POST'])
+
+@app.route("/query", methods=["POST"])
 def query():
     try:
-        query = request.json.get('query')
+        query = request.json.get("query")
         if not query:
             return jsonify({"error": "No query provided"}), 400
         data = query_database(query)
@@ -23,5 +25,6 @@ def query():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
